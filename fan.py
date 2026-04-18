5be64209d3ae423603e23d41509cf2600efffc5a
begeritos <72303665+3DMARTIAN@users.noreply.github.com>
2026-04-18 11:34:38 +0700

    Create fan.py

---
+++ fan.py
@@ -0,0 +1,11 @@
+def fan_info():
+    features = [
+        "Provides airflow",
+        "Consumes electricity",
+        "Used for cooling indoor spaces"
+    ]
+    for feature in features:
+        print(feature)
+
+if __name__ == "__main__":
+    fan_info()
